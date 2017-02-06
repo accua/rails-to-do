c0be57ea4385beafdb39a748dfcda8492031298e
Andrew Accuardi <andrewaccuardi@gmail.com>
2017-02-06 08:35:00 -0800

    Scaffolding, and some code, for rails to-do

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,16 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+    render :index
+  end
+
+  def show
+    @list = List.find(params[:id])
+    render :show
+  end
+
+  def new
+    @list = List.new
+    render :new
+end
